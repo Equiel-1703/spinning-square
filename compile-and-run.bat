49f81cb31870f5fa21b80b03f499b0de1814428b
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-12-07 17:13:33 -0300

    update: improved compile script and VSCode setup

--- compile-and-run.bat
+++ compile-and-run.bat
@@ -5,9 +5,10 @@ set obj-folder=objs
 set src-folder=src
 set program-name=spinning-square.exe
 
-cl.exe /EHsc /Fo: "%obj-folder%\\" /Fe: "%bin-folder%\\" %src-folder%\\*.cpp /link /out:%bin-folder%\\%program-name%
+cl.exe /EHsc /utf-8 /Fo: "%obj-folder%\\" /Fe: "%bin-folder%\\" %src-folder%\\*.cpp /link /out:%bin-folder%\\%program-name%
 
-if %errorlevel%==0 (
-	cls
+if %ERRORLEVEL% NEQ 0 (
+	exit /b 1
+) else (
 	%bin-folder%\\%program-name%
 )
