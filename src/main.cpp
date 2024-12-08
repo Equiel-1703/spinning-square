02ad4f797716ce7bbb0cd587ea7ba0abbf81ab22
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-12-08 00:48:59 -0300

    update: main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "../headers/Screen.hpp"
-#include "../headers/WinUtils.hpp"
+#include "Screen.hpp"
+#include "Square.hpp"
+#include "WinUtils.hpp"
 
 int main(int argc, char const *argv[])
 {
@@ -8,6 +9,14 @@ int main(int argc, char const *argv[])
 	CLEAR;
 
 	Screen screen(100, 40);
+	Square s(Vertice2D(0, 0), 10);
 
-	
+	s.draw(screen);
+
+	screen.showImage();
+
+	std::wcout << std::endl << std::endl;
+	PAUSE;
+
+	return 0;
 }
