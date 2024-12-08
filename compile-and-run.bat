535fc3910bf7c95fe3b30a5e19c2a2c06ca78fa0
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-12-08 00:46:00 -0300

    feat: added headers folder include in compile batch

--- compile-and-run.bat
+++ compile-and-run.bat
@@ -3,9 +3,10 @@
 set bin-folder=bin
 set obj-folder=objs
 set src-folder=src
+set headers-folder=headers
 set program-name=spinning-square.exe
 
-cl.exe /EHsc /utf-8 /Fo: "%obj-folder%\\" /Fe: "%bin-folder%\\" %src-folder%\\*.cpp /link /out:%bin-folder%\\%program-name%
+cl.exe /EHsc /utf-8 /Fo: "%obj-folder%\\" /Fe: "%bin-folder%\\" %src-folder%\\*.cpp /I "%headers-folder%" /link /out:%bin-folder%\\%program-name%
 
 if %ERRORLEVEL% NEQ 0 (
 	exit /b 1
